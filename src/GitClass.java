e614569490abede79cbca2d331abdb3dd8e5001e
vivekmksami <vivekjack22@gmail.com>
2022-08-05 10:24:56 +0530

    taskone

---
+++ src/GitClass.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class GitClass {
+	
+	
+		public static void main(String[] args) {
+			
+			Map<String, Integer> map = new HashMap<String, Integer>();
+			
+			map.put("csk",280);
+			map.put("kfc",220);
+			map.put("hdfc",230);
+			map.put("kgf",250);
+			map.put("sbi",235);
+			
+			System.out.println(map);
+			
+			int size = map.size();
+			System.out.println(size);
+
+}
+}
